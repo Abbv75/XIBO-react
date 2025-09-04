4ac25901dad9b2fea5cb7c522f2ae0669d9d8d9b
abbv75 <bore.younous59@gmail.com>
2025-09-04 17:34:11 +0000

    `Update JavaScript and CSS file references in asset manifest and index.html, and remove PrixProduitMarcheParRegion page from initial pages and import in PageAccueil ChargeurDePage component.`

--- src/constant/initialPages.js
+++ src/constant/initialPages.js
@@ -1,23 +1,12 @@
 import PageAccueil from "../pages/PageAccueil";
 import PrixCategorieParRegion from "../pages/PrixCategorieParRegion";
 import PrixMoyenProduitParRegion from "../pages/PrixMoyenProduitParRegion";
-import PrixProduitMarcheParRegion from "../pages/PrixProduitMarcheParRegion";
 
 export default [
     {
         id: "accueil",
         component: <PageAccueil />,
         duration: 30000,
-    },
-    {
-        id: "PrixProduitMarcheParRegion Oignon bulbe",
-        component: <PrixProduitMarcheParRegion produit="Oignon bulbe" />,
-        duration: 10000,
-    },
-    {
-        id: "PrixProduitMarcheParRegion Oignon Local",
-        component: <PrixProduitMarcheParRegion produit="Oignon Local" />,
-        duration: 10000,
     },
     {
         id: "PrixMoyenProduitParRegion Oignon bulbe",
--- src/pages/PageAccueil/ChargeurDePage.tsx
+++ src/pages/PageAccueil/ChargeurDePage.tsx
@@ -1,6 +1,7 @@
 import PrixProduitMarcheParRegion from '../PrixProduitMarcheParRegion'
 import PrixCategorieParRegion from '../PrixCategorieParRegion'
 import { memo } from 'react'
+import PrixMoyenProduitParRegion from '../PrixMoyenProduitParRegion'
 
 const ChargeurDePage = memo(() => {
     return (
